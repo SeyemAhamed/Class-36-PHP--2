5627a0848905ce68b80f37106392b33863440c03
SeyemAhamed <seyamjh@gmail.com>
2024-02-09 23:36:56 +0600

    PHP Loop & Function

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+
+// $x = 20;
+
+// if($x == 10){
+//     echo 'the value is=10';
+// }
+
+// else{
+//     echo 'The value is not=10';
+// }
+
+// $x = 10;
+
+// switch($x){
+// case 10;
+// echo 'The value is 10';
+// break; 
+
+// case 20;
+// echo 'The value is 20';
+// break;
+
+// case 30;
+// echo 'The value is 30';
+// break;
+
+// default:
+// echo 'None of the value is existing here';
+// break;
+// }
+
+// $array = [10, 20, 30, 40,34];   //index array
+
+// echo count($array). '<br>';
+
+// for ($i=0; $i<=4; $i++){
+//     echo $array[$i].'<br>';
+// }
+
+
+// foreach($array as $value){
+//     echo $value.'<br>';
+// }
+
+
+#include <stdio.h>
+
+int main() {
+
+    rtjutrj
+    
+        return 0;
+     }
+    
+    
+    function show ($number1, $number2, $number3){
+        return 'Hello world';  //30
+    }
+    
+    echo show(10, 20, 30);
+    // $result = show(10, 20);  //30
+    
+    // echo $result;
+    
+
+
+?>
